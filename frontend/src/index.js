cbb96f21b7677b940c97a2b671f434336172abc8
Sadeeshana <sadeeshanaakash16@gmail.com>
2025-11-15 17:16:22 +0530

    database connected

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from './Login';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
