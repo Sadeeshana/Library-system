00e1bac9518466ba9f589622fd26189fd9f667b4
Sadeeshana <sadeeshanaakash16@gmail.com>
2025-11-22 17:05:58 +0530

    Create again missing backend to in Dashboard 100% complete. All books card Book.java,BookController.java,BookRepository.java

---
+++ src/main/java/com/example/Library/system/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.Library.system;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    // JpaRepository already has a .count() method built-in!
+    // We don't need to write any SQL here for the total count.
+}
