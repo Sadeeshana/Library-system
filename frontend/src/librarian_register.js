78f311f2398acb370133ba97de5989dfd1b22609
Sadeeshana Akash <sadeeshanaakash16@gmail.com>
2025-11-25 20:40:21 +0530

    Mithila (#31)
    * librarian_register.css and librarian_register.js pages  100%
    
    ---------
    
    authored-by: Mithila Wijemanna <nmnwijemanna@students.nsbm.ac.lk>

---
+++ frontend/src/librarian_register.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./librarian_register.css";
+
+function LibrarianRegister() {
+    return (
+        <div className="container">
+
+            <div className="left-section">
+                <img src="/library-image.png" className="side-img" alt="Library" />
+            </div>
+
+            <div className="right-section">
+                <h2>Librarian Register</h2>
+
+                <form className="form">
+
+                    <label>Librarian ID</label>
+                    <input type="text" placeholder="Enter your ID" />
+
+                    <label>NIC</label>
+                    <input type="text" placeholder="Enter your NIC" />
+
+                    <label>E-mail</label>
+                    <input type="email" placeholder="Enter your E-mail" />
+
+                    <label>Phone Number</label>
+                    <input type="text" placeholder="Enter your Phone Number" />
+
+                    <label>Username</label>
+                    <input type="text" placeholder="Enter Username" />
+
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter Password" />
+
+                    <button className="register-btn">Register</button>
+                </form>
+
+            </div>
+        </div>
+    );
+}
+//Second push
+export default LibrarianRegister;
