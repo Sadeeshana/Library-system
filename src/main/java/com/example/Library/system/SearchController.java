9616d5a8d29f321eb664ac58a01623f305ece169
Abhishek955 <shelaranasinghe23@gmail.com>
2026-01-05 11:40:43 +0530

    Employee ,book, member instant details show
    Signed-off-by: Sadeeshana <sadeeshanaakash16@gmail.com>

---
+++ src/main/java/com/example/Library/system/SearchController.java
@@ -0,0 +1,53 @@
+package com.example.Library.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/search")
+public class SearchController {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //Search member
+    @GetMapping("/member/{id}")
+    public ResponseEntity<?> searchMember(@PathVariable int id) {
+        Optional<Member> member = memberRepository.findById(id);
+        if (member.isPresent()) {
+            return ResponseEntity.ok(member.get());
+        }
+        return ResponseEntity.accepted().body(Map.of("","Member not found in our records"));
+    }
+
+    //Search book
+    @GetMapping("/book/{id}")
+    public ResponseEntity<?> searchBook(@PathVariable Integer id) {
+        Optional<Book> book = bookRepository.findById(id);
+        if (book.isPresent()) {
+            return ResponseEntity.ok(book.get());
+        }
+        return ResponseEntity.accepted().body(Map.of("", "Book not found"));
+    }
+
+    //Search Employee
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<?> searchEmployee(@PathVariable Integer id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isPresent()) {
+            return ResponseEntity.ok(employee.get());
+        }
+        return ResponseEntity.accepted().body(Map.of("error", "Employee not found"));
+    }
+
+}
